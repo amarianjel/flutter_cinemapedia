965a933b6124b3b7b5fd4b78564b99b876c1ba07
amarianjel <abrm.enginner@gmail.com>
2023-09-26 16:36:28 -0300

    Funcionamiento del Boton

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -5,14 +5,27 @@ import '../../views/views.dart';
 class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex
+  });
+
+  final viewRoutes =  const <Widget>[
+    HomeView(),
+    SizedBox(), // <--- categorias View
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottomNavigation( currentIndex: pageIndex ),
     );
   }
 }
