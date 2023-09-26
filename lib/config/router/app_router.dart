9b69745e942afa05cc9964580bd91a13634b1bdd
amarianjel <abrm.enginner@gmail.com>
2023-09-26 16:07:30 -0300

    Favorite

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -7,21 +7,29 @@ final appRouter = GoRouter(
   routes: [
     
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-      routes: [
+      builder: (context, state) {
+        final pageIndex = int.parse( state.pathParameters['page'] ?? '0' );
 
+        return HomeScreen( pageIndex: pageIndex );
+      },
+      routes: [
         GoRoute(
           path: 'movie/:id',
           name: MovieScreen.name,
           builder: (context, state) {
             final movieId = state.pathParameters['id'] ?? 'no-id';
 
-            return MovieScreen( movieId: movieId);
+            return MovieScreen( movieId: movieId );
           },
         ),
-      ],
+      ]
+    ),
+
+    GoRoute(
+      path: '/',
+      redirect: ( _ , __ ) => '/home/0',
     ),
   ]
 );
