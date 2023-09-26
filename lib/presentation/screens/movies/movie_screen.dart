56ce1c8ca490d6c4646f366e047a51f319c837d7
amarianjel <abrm.enginner@gmail.com>
2023-09-26 11:35:13 -0300

    Pantalla terminada de la pelicula

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -27,6 +27,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
     
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
@@ -120,11 +121,73 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        // _ActorsByMovie(movieId: movie.id.toString() ),
+        _ActorsByMovie(movieId: movie.id.toString() ),
 
         const SizedBox(height: 50 ),
       ],
     );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+
+    if ( actorsByMovie[movieId] == null ) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                //* Actor Photo
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                //* Nombre
+                const SizedBox(height: 5,),
+
+                Text(actor.name, maxLines: 2 ),
+                Text(actor.character ?? '', 
+                  maxLines: 2,
+                  style: const TextStyle( fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+
   }
 }
 
